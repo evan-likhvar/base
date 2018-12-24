2e0c902e6f032ae74d89f565fffaea36e0cf86ea
Evan Likhvar <evan.likhvar@gcctechgroup.com>
2018-12-24 13:35:56 +0200

    merge validator erros to front message bag in BackUserController

--- app/Http/Controllers/Back/BackUserController.php
+++ app/Http/Controllers/Back/BackUserController.php
@@ -8,6 +8,7 @@ use App\Models\Role;
 use App\Repositories\SiteUser\BackUsers;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BackUserController extends BackController
 {
@@ -77,6 +78,8 @@ class BackUserController extends BackController
                 ->with([
                     'languages'=> Language::all()->pluck('full_name','id')->toArray(),
                     'roles' => Role::all()->pluck('name','id')->toArray(),
+                    'messages' => $this->frontMessage->toArray(),
+
                 ])
                 ->render());
         return $this->renderOutput();
@@ -84,12 +87,28 @@ class BackUserController extends BackController
 
     public function store(Request $request)
     {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'language_id' =>'required|exists:languages,id',
+            'active' => 'required|in:0,1',
+            'dashboard_enable' => 'required|in:0,1',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            $this->addErrorMessage(['errors' => $validator->getMessageBag()->toArray()]);
+            return redirect()->back()->withInput();
+        }
+
+
         $user = User::create($request->all());
         $user->roles()->sync(array_keys($request['roles']));
 
         $this->addFrontMessage(['message' => "User <b>$user->full_name</b> created successfully"]);
 
-        return redirect()->route('backend.user.index')->with('frontMessageBag',$this->frontMessage);
+        return redirect()->route('backend.user.index');
     }
 
     public function update(Request $request, int $userId)
